41d4c940443187e886798bc3a9164cd370f748f9
chilkatsoft <admin@chilkatsoft.com>
2015-12-07 11:31:37 -0600

    initial files

---
+++ Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim sftp As New Chilkat.SFtp()
+
+        Dim success As Boolean
+
+        success = sftp.UnlockComponent("30-day trial")
+        TextBox1.Text = sftp.LastErrorText
+
+
+    End Sub
+End Class
